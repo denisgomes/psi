3d2a3afdcef45aa10ec85c5f8a6b56ec5d37e04d
Denis Gomes <denis.gomes@consultant.com>
2021-07-11 11:24:21 -0400

    Created basic layout for user interface

--- psi/bin/gui.py
+++ psi/bin/gui.py
@@ -3,28 +3,105 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
+from psi.gui.ribbon import Ribbon
+from psi.gui.graphics.pyglettk import PygletTkFrame
 
-class Application(ttk.Frame):
+
+class GUI(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.pack()
+        self._create_widgets()
+        self._do_layout()
 
-        self.create_widgets()
-        self.do_layout()
-
-    def create_widgets(self):
+    def _create_widgets(self):
         # make ribbon
-        # pyglettk area
-        # make tables
-        # make status
-        pass
+        self.ribbon = Ribbon(self)
 
-    def do_layout(self):
-        pass
+        # pyglettk area
+        self.graphics = PygletTkFrame(self)
+
+        # icon frame
+        self.icon_bar = IconBar(self)
+
+        # make tables, interp, etc...
+        self.footer = ttk.Frame(self, height=144)
+
+        # make status
+        self.status_bar = StatusBar(self)
+
+    def _do_layout(self):
+        self.ribbon.pack(fill=tk.X)
+        self.icon_bar.pack(fill=tk.X)
+        self.graphics.pack(expand=True, fill=tk.BOTH)
+        self.footer.pack(fill=tk.X)
+        self.status_bar.pack(fill=tk.X)
+
+        self.pack(expand=True, fill=tk.BOTH)
+
+
+class Footer(ttk.Frame):
+
+    def __init__(self, parent):
+        super(Footer, self).__init__(parent)
+        self.parent = parent
+
+        self._create_widgets()
+        self._do_layout()
+
+    def _create_widgets(self):
+        self.data_notebook_frame = ttk.Frame(self, height=144)
+        self.data_notebook = ttk.Notebook(self.data_notebook_frame)
+
+        self.data_notebook_element_frame = ttk.Frame(self.data_notebook)
+        self.data_notebook.add(self.data_notebook_element_frame, text="Element")
+
+    def _do_layout(self):
+        self.data_notebook_element_frame.pack(fill=tk.X)
+
+        self.data_notebook.pack(fill=tk.X)
+        self.data_notebook_frame.pack(fill=tk.X)
+
+        self.pack(expand=True, fill=tk.X)
+
+
+class StatusBar(ttk.Frame):
+
+    def __init__(self, parent):
+        super(StatusBar, self).__init__(parent)
+        self.parent = parent
+
+        self._create_widgets()
+        self._do_layout()
+
+    def _create_widgets(self):
+        self.notify_label = ttk.Label(self)
+
+    def _do_layout(self):
+        self.notify_label.pack(expand=True, fill=tk.X)
+
+
+class IconBar(ttk.Frame):
+
+    def __init__(self, parent):
+        super(IconBar, self).__init__(parent)
+        self.parent = parent
+
+        self._create_widgets()
+        self._do_layout()
+
+    def _create_widgets(self):
+        self.single_select_button = ttk.Button(self)
+        self.sep1 = ttk.Separator(self, orient="vertical")
+        self.range_select_button = ttk.Button(self)
+
+    def _do_layout(self):
+        self.single_select_button.pack(side=tk.LEFT, padx=1, pady=1)
+        self.sep1.pack(side=tk.LEFT, fill=tk.Y, padx=3, pady=3)
+        self.range_select_button.pack(side=tk.LEFT, padx=1, pady=1)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = Application(master=root)
+    app = GUI(master=root)
     root.title("PS8")
     root.mainloop()
