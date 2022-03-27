1239d096d8ecdbe0c760c222bcf839954f96e3c5
Denis Gomes <denisgomes@consultant.com>
2022-03-26 23:15:41 -0400

    Added springs and templates to package data

--- setup.py
+++ setup.py
@@ -3,22 +3,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="psi",
-
     version="0.0.1",
-
     description=("Pipe Stress Infinity (PSI) - The pipe stress analysis and "
                  "design software."),
-
     # display on pypi
     long_description=open("README.rst", "r").read(),
-
     url="https://www.github.com/denisgomes/psi",
-
     author="Denis Gomes",
-
     author_email="denis.gomes@consultant.com",
-
     license="GPLv3",
+    project_urls={
+        "Documentation": "https://pipe-stress-infinity.readthedocs.io/en/latest",
+        "Source": "https://github.com/denisgomes/psi",
+        "Tracker": "https://github.com/denisgomes/psi/issues"
+    },
 
     # advertise program attributes
     classifiers=[
@@ -52,6 +50,14 @@ setup(
         "psi.data.springs": ["anvil_variable.csv", "anvil_constant.csv"],
         "psi.data.units": ["base.csv", "english.csv", "code_english.csv",
                            "si.csv"],
+        "psi.templates": ["base_report", "single_case_report",
+            "multiple_case_report", "single_case_movements",
+            "single_case_forces", "single_case_reactions",
+            "single_case_stresses", "single_case_stresses_extended",
+            "single_case_codes", "multiple_case_movements",
+            "multiple_case_forces", "multiple_case_reactions",
+            "multiple_case_stresses", "multiple_case_stresses_extended",
+            "multiple_case_codes" ]
         },
 
     # MANIFEST.in works for source distributions only
